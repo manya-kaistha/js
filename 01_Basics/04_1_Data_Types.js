935c69028fc1497167ac2d766cb1acf095952cde
manya-kaistha <130305667+manya-kaistha@users.noreply.github.com>
2024-10-29 19:11:35 +0000

    Data Type Summary

---
+++ 01_Basics/04_1_Data_Types.js
@@ -0,0 +1,29 @@
+// Primitive 7 types: Biglnt, String,Number,Boolean,null,undefined,Symbol
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+/*
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
