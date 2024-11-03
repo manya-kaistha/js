195ed3868e96ed522f4cd770d0717131884fcac6
manya-kaistha <130305667+manya-kaistha@users.noreply.github.com>
2024-11-03 12:19:39 +0000

    Basic Functions complete

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -23,3 +23,31 @@ function loginUserMessage(username = "sam"){ // this is basically giving a defau
     return `${username} just logged in`
 }
 console.log(loginUserMessage("manya"))
+
+function calculateCartPrice(val1, val2, ...num1){ //... is a rest operator
+    return num1
+}
+
+console.log(calculateCartPrice(100, 200, 300, 400))// this will give us an array becaouse of the rest operatorthat can be iterated through
+// the first 2 values will go to val1 and val2 and all others will go the array(...num1)
+
+const user = {
+    username: "manya",
+    price: 99
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and the price is ${anyObject.price}`)
+}
+handleObject(user)
+
+handleObject({  // you can also create a user and pass that
+    username: "manya",
+    price: 99
+})
+
+const myNewArray = [100, 200, 300, 400]
+function ReturnSecondValue(anyArray){
+    return anyArray[1]
+}
+console.log(ReturnSecondValue(myNewArray))
