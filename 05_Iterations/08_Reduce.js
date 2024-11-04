ef52cd257ccb1526455b57c2564f0144ab7ad34f
manya-kaistha <130305667+manya-kaistha@users.noreply.github.com>
2024-11-04 09:43:20 +0000

    Reduce

---
+++ 05_Iterations/08_Reduce.js
@@ -0,0 +1,32 @@
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`Acc: ${acc} and Currval: ${currval}`)
+    return acc + currval
+}, 0)
+console.log(myTotal)
+
+const myTotal2 = myNums.reduce( (acc, currval) => acc + currval, 0)
+console.log(myTotal2)
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay)
